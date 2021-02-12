8c4707695c5f96b5973ddc0d39905bcf8f1b8169
fkk <kinako.fukiko.0822@gmail.com>
2021-02-12 15:14:32 +0900

    add children of blog_index

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,15 +1,40 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
-
+# ツリー構造の中で、幹というか茎？の部分(node)
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('intro', classname="full")
+    # ]
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at') # live()で、publishされているものだけ表示
+        context['blogpages'] = blogpages
+        return context
+
+# ツリー構造の中で、葉の部分
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
     content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
     ]
 
 """Doc Page Models"""
